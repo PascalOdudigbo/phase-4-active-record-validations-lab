6b65c23de022e66ec7ad4bc937a1aea3cc576726
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-10-04 09:26:23 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    #validate :title_clickbaity
+
+    # def title_clickbaity
+    #     if !title.include? "Won't Believe" || !title.include? "Secret" || !title.include? "Top" || !title.include? "Guess"
+    #         errors.add(:title, "must be clickbait")
+    #     end
+    # end
 end
